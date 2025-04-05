6423c28651727b2f3bd7dddf5a43f8ae0d7e39df
Carter <speers.carter@gmail.com>
2025-04-05 14:57:03 -0400

    added functions

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 
-template = """Question: {question}
+import json
+import requests
+import os
 
-Answer: Let's think step by step."""
+access_token = os.environ.get('CANVAS_API_ACCESS_TOKEN')
+api_url = "https://usflearn.instructure.com/api/v1/courses"
+headers = {"Authorization": f"Bearer {access_token}"}
 
-prompt = ChatPromptTemplate.from_template(template)
+class Course:
+    def __init__(self, course_id, name):
+        self.course_id = course_id
+        self.name = name
+        
+    def __str__(self):
+        return f"Course ID: {self.course_id}, Name: {self.name}" 
 
-model = OllamaLLM(model="gemma3:1b")
 
-chain = prompt | model
+def get_course_list():
+    response = requests.get(api_url, headers=headers)
+    course_list = []
 
-print(chain.invoke({"question": "What is LangChain?"}))
+    if response.status_code == 200:
+        courses = response.json()
+        if courses:
+            for course in courses:
+               course_list.append(Course(course['id'], course['name'])) 
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+    return course_list
+
+
+def prompt_llm(question):
+    template = """Question: {question}
+    Answer: Let's think step by step."""
+    prompt = ChatPromptTemplate.from_template(template)
+    model = OllamaLLM(model="gemma3:1b")
+    chain = prompt | model
+    return chain.invoke({"question": "What is LangChain?"})
+
+
+if __name__ == "__main__":
+    courses = get_course_list()
+    for course in courses:
+        print(course)
